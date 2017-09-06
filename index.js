bec0f90ed721dcd00d32a43a2eb5c5557f7ec8aa
JM Cléry <jmclery@gmail.com>
2017-09-06 12:30:36 +0200

    Ajoute la connexion socket.io

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const path    = require('path')
-const express = require('express')
-const stylus  = require('stylus')
+const path       = require('path')
+const express    = require('express')
+const stylus     = require('stylus')
+const bodyParser = require('body-parser')
 
-const app     = express()
+const app    = express()
+const server = require('http').Server(app)
+const io     = require('socket.io')(server);
 
 /**
  * App configuration
@@ -20,6 +23,7 @@ app.use(stylus.middleware({
     src  : path.resolve('static/styles'), // Les fichiers .styl se trouvent dans `static/styles`
     dest : path.resolve('static/styles') // Les fichiers .styl sont également compilés en CSS dans `static/styles`
 }))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.resolve('static')))
 
 /**
@@ -27,11 +31,23 @@ app.use(express.static(path.resolve('static')))
  */
 
 app.get('/', (request, response) => {
-    response.render('master', {pageName:'home'})
+    response.render('index')
+})
+
+app.post('/chat', (request, response) => {
+    response.render('chat', {
+        user: {
+            name: request.body.pseudo
+        }
+    })
+})
+
+io.on('connection', (socket) => {
+    console.info('Un client a ouvert un socket sur notre chat !', socket.id);
 })
 
 /**
  * App start
  */
 
-app.listen(app.get('port'), () => console.log(`App listen on port ${app.get('port')}`))
+server.listen(app.get('port'), () => console.log(`App listen on port ${app.get('port')}`))
