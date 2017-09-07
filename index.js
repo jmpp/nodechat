9d3173fc93507d34f37734f1091d2a5d34377073
JM Cléry <jmclery@gmail.com>
2017-09-07 10:24:46 +0200

    Ajoute un vrai système de connexion avec express-session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path       = require('path')
 const express    = require('express')
 const stylus     = require('stylus')
 const bodyParser = require('body-parser')
+const session    = require('express-session')
 
 const app    = express()
 const server = require('http').Server(app)
@@ -19,6 +20,12 @@ app.set('port', 3000)
  * App middlewares
  */
 
+app.use(session({
+    secret           : 'node meow',
+    cookie           : { maxAge: 60000 },
+    resave           : false,
+    saveUninitialized: false
+}))
 app.use(stylus.middleware({
     src  : path.resolve('static/styles'), // Les fichiers .styl se trouvent dans `static/styles`
     dest : path.resolve('static/styles') // Les fichiers .styl sont également compilés en CSS dans `static/styles`
@@ -26,25 +33,44 @@ app.use(stylus.middleware({
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.resolve('static')))
 
+// Security middleware for route "/chat"
+ app.use('/chat', (request, response, next) => {
+    if (!request.session.user)
+        return response.redirect(302, '/')
+
+    next()
+}) 
+
 /**
  * App routes
  */
 
-app.get('/', (request, response) => {
+app.get('/logout', (request, response) => {
+    request.session.destroy(() => response.redirect(301, '/'))
+})
+
+ app.get('/', (request, response) => {
     response.render('index')
 })
 
-app.post('/chat', (request, response) => {
+app.post('/login', (request, response) => {
+    // Create session
+    request.session.user = {
+        name : request.body.pseudo
+    }
+
+    response.redirect(302, '/chat');
+})
+
+app.get('/chat', (request, response) => {
     response.render('chat', {
-        user: {
-            name: request.body.pseudo
-        }
+        user: request.session.user
     })
 })
 
 io.on('connection', (socket) => {
     console.info('Un client a ouvert un socket sur notre chat !', socket.id);
-})
+}) 
 
 /**
  * App start
