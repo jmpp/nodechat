04ef96398fc8c373f97e71d7b55d4c95696cf736
JM Cléry <jmclery@gmail.com>
2017-09-07 17:37:07 +0200

    Permet de chatter avec plusieurs utilisateurs

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const path       = require('path')
-const express    = require('express')
-const stylus     = require('stylus')
-const bodyParser = require('body-parser')
-const session    = require('express-session')
+const path          = require('path')
+const express       = require('express')
+const stylus        = require('stylus')
+const bodyParser    = require('body-parser')
+const session       = require('express-session')
+const sharedSession = require('express-socket.io-session')
 
 const app    = express()
 const server = require('http').Server(app)
-const io     = require('socket.io')(server);
+const io     = require('socket.io')(server)
+
+const sessionObj = session({
+                             secret : 'node meow',
+                             cookie : { maxAge: 500000 }, // 5 min
+                             resave : false,
+                             saveUninitialized : false
+                          })
+const sharedSessionObj = sharedSession(sessionObj, { autoSave : true })
 
 /**
  * App configuration
@@ -20,12 +29,8 @@ app.set('port', 3000)
  * App middlewares
  */
 
-app.use(session({
-    secret           : 'node meow',
-    cookie           : { maxAge: 60000 },
-    resave           : false,
-    saveUninitialized: false
-}))
+app.use(sessionObj)
+io.use(sharedSessionObj)
 app.use(stylus.middleware({
     src  : path.resolve('static/styles'), // Les fichiers .styl se trouvent dans `static/styles`
     dest : path.resolve('static/styles') // Les fichiers .styl sont également compilés en CSS dans `static/styles`
@@ -33,13 +38,13 @@ app.use(stylus.middleware({
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static(path.resolve('static')))
 
-// Security middleware for route "/chat"
+// Middleware custom qui s'assure que l'on a le droit d'accèder à la route : GET /chat
  app.use('/chat', (request, response, next) => {
     if (!request.session.user)
         return response.redirect(302, '/')
 
     next()
-}) 
+})
 
 /**
  * App routes
@@ -49,31 +54,64 @@ app.get('/logout', (request, response) => {
     request.session.destroy(() => response.redirect(301, '/'))
 })
 
- app.get('/', (request, response) => {
+app.get('/', (request, response) => {
     response.render('index')
 })
 
 app.post('/login', (request, response) => {
-    // Create session
+    // Crée la session
     request.session.user = {
-        name : request.body.pseudo
+        pseudo : request.body.pseudo
     }
-
-    response.redirect(302, '/chat');
+    response.redirect(302, '/chat')
 })
 
 app.get('/chat', (request, response) => {
-    response.render('chat', {
-        user: request.session.user
-    })
+    response.render('chat')
 })
 
-io.on('connection', (socket) => {
-    console.info('Un client a ouvert un socket sur notre chat !', socket.id);
+io.on('connection', socket => {
+
+    // Si aucun utilisateur n'existe dans la session en cours, on déconnecte le socket et on s'arrête là
+    if (!socket.handshake.session.user)
+        return socket.disconnect(true)
+
+    // Création de l'objet représentant cet utilisateur
+    let user = {
+        pseudo  : socket.handshake.session.user.pseudo,
+        socketId : socket.id
+    }
+
+    // Ajout à la liste tenue par le serveur
+    USERS.push(user)
+
+    // Envoi à ce client de ses informations, pour que le JS côté client puisse les exploiter dans le HTML
+    socket.emit('userInfos', user)
+
+    // Envoie à tous les clients la nouvelle liste d'utilisateurs
+    io.sockets.emit('updateUserList', USERS.map(u => u.pseudo))
+
+    // Quand on reçoit un message de ce client, on redispatche aux autres
+    socket.on('message', message => {
+        // Ici, on s'assure que quelquesoit le pseudo envoyé (e.g. si le client à bidouillé le socket) que l'on va bien chercher le pseudo associé à ce socket.id dans la liste gérée par le serveur
+        message.pseudo = USERS.find(u => u.socketId === socket.id).pseudo
+
+        // Dispatche le message à tous les autres clients
+        socket.broadcast.emit('message', message)
+    })
+
+    // Quand ce client se déconnecte, maj de la liste côté serveur, qu'on envoie ensuite aux clients connectés
+    socket.on('disconnect', () => {
+        USERS = USERS.filter(u => u.socketId !== socket.id)
+
+        socket.broadcast.emit('updateUserList', USERS.map(u => u.pseudo))
+    })
 }) 
 
 /**
  * App start
  */
+
+let USERS = []
 
 server.listen(app.get('port'), () => console.log(`App listen on port ${app.get('port')}`))
--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -1,6 +1,64 @@
 (function() {
     'use strict';
 
-    // var socket = io.connect('/')
+    var vm = new Vue({
+        el : 'main',
+        data : function() {
+            return {
+                socket      : null,
+                user        : {},
+                messages    : [],
+                userList    : [],
+                messageText : ''
+            }
+        },
+        methods : {
+            init : function() {
+                this.socket = io.connect('/');
+
+                this.socket.on('message', this.onMessage.bind(this));
+                this.socket.on('userInfos', this.onUserInfos.bind(this));
+                this.socket.on('updateUserList', this.onUpdateUserList.bind(this));
+            },
+
+            postMessage : function() {
+                // Création de l'objet message
+                var message = {
+                    pseudo : this.user.pseudo,
+                    text : this.messageText,
+                    dateCreated : Date.now()    
+                };
+
+                // Ajout à la liste de messages Vue.js
+                this.messages.push(message);
+
+                // Envoi du message au serveur, pour qu'il le re-dispatche aux autres
+                this.socket.emit('message', message);
+
+                // Reset du champs de formulaire
+                this.messageText = '';
+            },
+
+            // A la réception d'un message du serveur, on l'ajoute à la liste Vue.js
+            onMessage : function(data) {
+                this.messages.push(data);
+            },
+
+            // Le serveur nous renvoie les informations concernant notre profil (pseudo et socketId) pour les utiliser dans l'interface HTML/CSS
+            onUserInfos : function(userInfos) {
+                this.user = userInfos;
+            },
+
+            // Le serveur envoie une maj de la liste des utilisateurs connectés, qu'on recharge avec Vue.js
+            onUpdateUserList : function(userList) {
+                this.userList = userList;
+            }
+        },
+        mounted : function() {
+            this.$refs.message.focus();
+
+            this.init();
+        }
+    })
 
 })();
